52d3b06cbdf967a9b3c4fd445700ead849563dd0
MerkuchevAntonPIE23 <152679844+MerkuchevAntonPIE23@users.noreply.github.com>
2023-12-23 07:42:48 +0700

    Delete scripts directory

--- scripts/script1.js
+++
@@ -1,6 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
-  asd = document.getElementById('json')
-  asd.innerHTML = json 
